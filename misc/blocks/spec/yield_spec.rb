f942ef1b2e38b56303f78a941853bad86ed730ee
chrismo <chrismo@clabs.org>
2017-07-22 23:13:47 -0500

    old leftovers

---
+++ misc/blocks/spec/yield_spec.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+require File.dirname(__FILE__) + '/../lib/yield'
+
+describe What do
+
+  it "should add return from yield into the sum"
+
+  it "should [something like cope with the resource at the end of the block]"
+
+  it "should not require a block"
+
+end
